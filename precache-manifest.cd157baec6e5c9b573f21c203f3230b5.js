0bd28a78da85ec2bfaece2094a7059d0dfae0b25
Code Sandbox <hello@codesandbox.io>
2021-06-03 12:17:15 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.cd157baec6e5c9b573f21c203f3230b5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c4f0a0dcd3f9fbe7843e0cc0ec9e67",
+    "url": "https://desiringsubjects.github.io/csb-mocdd/index.html"
+  },
+  {
+    "revision": "c5dc56f8982c51ad958b",
+    "url": "https://desiringsubjects.github.io/csb-mocdd/static/css/main.d0abaa30.chunk.css"
+  },
+  {
+    "revision": "e9c72fbd8f56c10f100c",
+    "url": "https://desiringsubjects.github.io/csb-mocdd/static/js/2.2497ef8c.chunk.js"
+  },
+  {
+    "revision": "c5dc56f8982c51ad958b",
+    "url": "https://desiringsubjects.github.io/csb-mocdd/static/js/main.208ff85a.chunk.js"
+  },
+  {
+    "revision": "0a2f404b092d8f11afd9",
+    "url": "https://desiringsubjects.github.io/csb-mocdd/static/js/runtime~main.73072aa3.js"
+  }
+]);
